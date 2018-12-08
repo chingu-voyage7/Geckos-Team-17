9c4362ca9ac5f7917ff05571ad84dfd5767f226b
Anshul Dubey <anshuldubey2166@gmail.com>
2018-12-08 15:00:57 +0530

    Added Header

--- petmanager/client/src/components/App/App.js
+++ petmanager/client/src/components/App/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Header from "../Header/Header";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <h1>This is the App page</h1>
       </div>
     );
