8a9c4df24b8da223307caa198e3e2078fd5ffe2d
HixOba <hi.savis@gmail.com>
2018-12-19 13:15:45 +0000

    Add basic router

--- petmanager/client/src/App.js
+++ petmanager/client/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 import './bulmastyles.css';
  
@@ -8,14 +11,19 @@ import Categories from './app/layout/Categories';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-       <h1>PetMan</h1>
-      <Categories />  
-      
-      </div>
+      <Router>
+        <div className="App">
+          <h1>PetMan</h1>
+          <Route exact path="/" component={ Categories } />  
+          <div className="container">
+            <Route exact path="/register" component={ Register } />
+            <Route exact path="/login" component={ Login } />
+          </div>
+         </div>
+      </Router>
     ); 
   }
 }
- 
+  
 export default App;
  
