4379a0dbef8dbb683e971c320da360161dcb26eb
Anshul Dubey <anshuldubey2166@gmail.com>
2018-12-21 12:32:23 +0530

    Aligned the image and text

--- petmanager/client/src/components/Profile/Profile.js
+++ petmanager/client/src/components/Profile/Profile.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import './profile.css';
+import "./profile.css";
 
 class Profile extends Component {
   render() {
@@ -7,22 +7,23 @@ class Profile extends Component {
       <div className="profile">
         <div class="container is-fluid">
           <div class="columns">
-            <div class="column is-one-third">
-              <div class="card-image">
-                <figure class="image is-4by3">
+            <div class="column is-one-fifth">
+              <div class="card-image image-column">
+                <figure class="image image-center">
                   <img
-                    src="https://bulma.io/images/placeholders/1280x960.png"
-                    alt="profile pic"
+                    class="is-rounded image-img right-align"
+                    src="https://bulma.io/images/placeholders/128x128.png"
+                    alt="Placeholder"
                   />
                 </figure>
               </div>
             </div>
-            <div class="column is-two-fifths">
+            <div class="column is-two-fifths left-align">
               <div class="column">
                 <h3 class="title is-3">Anshul Dubey</h3>
               </div>
               <div class="column">
-                <i class="fas fa-map-marker fa-fw left-align"/>
+                <i class="fas fa-map-marker fa-fw left-align" />
                 H-3 Krishna Nagar
               </div>
               <div class="column">
