47884a24210e78bb8397e3a3ab2b30f5d678ee0e
Anshul Dubey <anshuldubey2166@gmail.com>
2019-01-03 22:18:01 +0530

    Removed login

--- petmanager/client/src/App.js
+++ petmanager/client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Register from './components/auth/Register';
-import Login from './components/auth/Login';
 import Calendar from "./components/calendar/Calendar";
 import Profile from "./components/profile/Profile";
 import Header from "./components/header/Header";
