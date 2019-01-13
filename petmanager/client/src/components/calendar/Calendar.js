39bb67b5b97772c1485878f5bf1a7270931c8598
Anshul Dubey <anshuldubey2166@gmail.com>
2019-01-13 12:41:26 +0530

    Added to-do modal

--- petmanager/client/src/components/calendar/Calendar.js
+++ petmanager/client/src/components/calendar/Calendar.js
@@ -5,6 +5,7 @@ import events from "./events";
 import Modal from "react-responsive-modal";
 import DatePicker from "react-datepicker";
 import TimePicker from "react-time-picker";
+import DateTimePicker from "react-datetime-picker";
 const localizer = BigCalendar.momentLocalizer(moment);
 
 class Calendar extends Component {
@@ -60,7 +61,6 @@ class Calendar extends Component {
           onClose={this.onCloseModal}
           className="calendar-modal"
           center
-          showCloseIcon={false}
         >
           Details for the event:-
           <div>
@@ -81,17 +81,26 @@ class Calendar extends Component {
           className="calendar-modal"
           center
           showCloseIcon={false}
+          blockScroll={false}
         >
-          <div>
-            Date:&nbsp;
-            <DatePicker
-              selected={this.state.startDate}
-              onChange={this.handleChange}
+          <div className="row title">Add a to-do</div>
+          <div className="row">
+            <p>Title</p>
+            <input type="text" />
+          </div>
+          <div className="row">
+            <p>Description</p>
+            <textarea rows="6" />
+          </div>
+          <div className="row">
+            <p>Time:&nbsp;</p>
+            <DateTimePicker
+              onChange={this.onChange}
+              value={this.state.startDate}
             />
           </div>
-          <div>
-            Time:&nbsp;
-            <TimePicker onChange={this.onChange} value={this.state.time} />
+          <div className="row has-text-centered">
+            <button class="button">Add</button>
           </div>
         </Modal>
       </div>
