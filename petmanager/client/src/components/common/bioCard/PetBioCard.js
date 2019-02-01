1fe0806a67f7f2400a38a46671ea04aa35e6fc04
HixOba <hi.savis@gmail.com>
2019-02-01 21:28:14 +0000

    Add pets bio card template

---
+++ petmanager/client/src/components/common/bioCard/PetBioCard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import FontAwesome from 'react-fontawesome';
+import styles from '../bioCard/bioCard.css';
+
+const PetBioCard = (props) => {
+  return (
+    <div className={styles.PetBioCard}>
+      <span className={styles.ownerNamePhone}>
+        nicky
+      </span>
+      <span className={styles.dateCalc}>
+        <FontAwesome name="clock-o" />
+        {props.dateCalc} 
+      </span>
+      <span className={styles.petData}>
+      </span>
+    </div>
+  );
+};
+
+export default PetBioCard; 
