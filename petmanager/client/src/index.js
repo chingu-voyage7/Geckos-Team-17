6774d7e68f1f82ac1ef0ae41b8c925d5e18e33ec
HixOba <hi.savis@gmail.com>
2019-02-08 13:11:11 +0000

    Rebuild bulma

--- petmanager/client/src/index.js
+++ petmanager/client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; 
 import 'bulma/css/bulma.css';
+import './bulmastyles.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import "react-datepicker/dist/react-datepicker.css";
