25430d27236fe83f23a24c3f0676330e6bb280d1
HixOba <hi.savis@gmail.com>
2019-02-12 11:49:37 +0000

    Update header title - basic

--- petmanager/client/src/components/header/Header.js
+++ petmanager/client/src/components/header/Header.js
@@ -6,13 +6,9 @@ class Header extends Component {
       <div className="header">
         <nav class="navbar" role="navigation" aria-label="main navigation">
           <div class="navbar-brand">
-            <a class="navbar-item" href="https://bulma.io">
-              <img
-                src="https://bulma.io/images/bulma-logo.png"
-                width="112"
-                height="28"
-                alt="title"
-              />
+            <a>
+            <h3 class="title has-text-blue is-grouped-left">PetMan</h3>
+              
             </a>
 
             <a
